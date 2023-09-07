936829ee736722b3effe3430f07a6769704aa076
yinhuasheng <yinhuasheng@unicloud.com>
2023-09-07 16:16:52 +0800

    初始化项目

---
+++ src/main/java/com/coderzoe/tcpbasedudp/tcp/TcpSegmentStruct.java
@@ -0,0 +1,9 @@
+package com.coderzoe.tcpbasedudp.tcp;
+
+/**
+ * @author yinhuasheng
+ * @email yinhuasheng@unicloud.com
+ * @date 2023/9/7 16:14
+ */
+public class TcpSegmentStruct {
+}
---
+++ src/test/java/com/coderzoe/tcpbasedudp/TcpBasedUdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderzoe.tcpbasedudp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpBasedUdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
